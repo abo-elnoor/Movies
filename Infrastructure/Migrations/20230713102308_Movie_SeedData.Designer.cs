ea7ab14ac74bd51e92db3a96583f9450c614cd22
Mahmoud Mohamed Abd Al azeem <mahmoud-mohamed@outlook.com>
2023-07-13 13:24:04 +0300

    Add seed Data

--- Infrastructure/Migrations/20230713102308_Movie_SeedData.Designer.cs
+++ Infrastructure/Migrations/20230713102308_Movie_SeedData.Designer.cs
@@ -2,6 +2,7 @@
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(SqliteDbContext))]
-    partial class SqliteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230713102308_Movie_SeedData")]
+    partial class Movie_SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.9");
--- Infrastructure/SqliteDbContext.cs
+++ Infrastructure/SqliteDbContext.cs
@@ -14,13 +14,7 @@ namespace Infrastructure
 {
     public class SqliteDbContext : DbContext, IDbContext
     {
-        //protected readonly IConfiguration Configuration;
-        //public SqliteDbContext(IConfiguration configuration)
-        //{
-        //    Configuration = configuration;
-        //}
-
-        public SqliteDbContext(DbContextOptions<SqliteDbContext> options) : base(options){}
+        public SqliteDbContext(DbContextOptions<SqliteDbContext> options) : base(options) { }
 
         public new DbSet<TEntity> Set<TEntity>() where TEntity : BaseEntity
         {
@@ -31,11 +25,31 @@ namespace Infrastructure
         public DbSet<Person> Person { get; set; }
         public DbSet<Cast> Cast { get; set; }
 
-       
-        //protected override void OnConfiguring(DbContextOptionsBuilder options)
-        //{
-        //    // connect to sqlite database
-        //    options.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
-        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie() { Id = 1, Title = "Movie1", DirectorId = 1 },
+                new Movie() { Id = 2, Title = "Movie2", DirectorId = 2 });
+
+            modelBuilder.Entity<Person>().HasData(
+              new Person() { Id = 1, Name = "Director1", Gender = 'F' },
+              new Person() { Id = 2, Name = "Director2", Gender = 'M' },
+              new Person() { Id = 3, Name = "Person3", Gender = 'F' },
+              new Person() { Id = 4, Name = "Person4", Gender = 'M' },
+              new Person() { Id = 5, Name = "Person5", Gender = 'M' },
+              new Person() { Id = 6, Name = "Person6", Gender = 'F' },
+              new Person() { Id = 7, Name = "Person7", Gender = 'M' },
+              new Person() { Id = 8, Name = "Person8", Gender = 'M' });
+
+            modelBuilder.Entity<Cast>().HasData(
+               new Cast() { Id = 1, PersonId = 3, CharName = "Cast1", MovieId = 1, CastOrder = 1, CharGender = 'F' },
+               new Cast() { Id = 2, PersonId = 4, CharName = "Cast2", MovieId = 1, CastOrder = 1, CharGender = 'M' },
+               new Cast() { Id = 3, PersonId = 5, CharName = "Cast3", MovieId = 1, CastOrder = 2, CharGender = 'F' },
+               new Cast() { Id = 4, PersonId = 6, CharName = "Cast4", MovieId = 2, CastOrder = 1, CharGender = 'F' },
+               new Cast() { Id = 5, PersonId = 7, CharName = "Cast5", MovieId = 2, CastOrder = 2, CharGender = 'M' },
+               new Cast() { Id = 6, PersonId = 8, CharName = "Cast6", MovieId = 2, CastOrder = 3, CharGender = 'M' }
+               );
+        }
     }
+
 }
